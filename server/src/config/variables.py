e04b2293874347efc4768928bbfaf83d6b1a83c5
josethz00 <jtsoares17@hotmail.com>
2021-02-03 21:14:29 -0300

    modified: variables.py added new env variable: REDIS_URL

--- server/src/config/variables.py
+++ server/src/config/variables.py
@@ -3,7 +3,9 @@ import os
 os.environ['HOST'] = '0.0.0.0'
 os.environ['PORT'] = '8080'
 os.environ['DEBUG'] = 'True'
+os.environ['REDIS_URL'] = 'redis://localhost:6379/0'
 
 HOST = os.environ.get('HOST')
 PORT = os.environ.get('PORT')
 DEBUG = os.environ.get('DEBUG')
+REDIS_URL = os.environ.get('REDIS_URL')
