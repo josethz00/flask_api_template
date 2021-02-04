5807384ff96ed24b18bc87f4b8c8656c46744b18
josethz00 <jtsoares17@hotmail.com>
2021-02-03 21:13:51 -0300

    modified: app.py added redis connection through env variables

--- server/app.py
+++ server/app.py
@@ -6,8 +6,8 @@ from src.config import variables
 app = Flask(__name__)
 CORS(app)
 
-app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
-app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
+app.config['CELERY_BROKER_URL'] = variables.REDIS_URL
+app.config['CELERY_RESULT_BACKEND'] = variables.REDIS_URL
 
 celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
 celery.conf.update(app.config)
